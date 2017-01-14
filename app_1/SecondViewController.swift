1e30bf38b6d44067071ecaddc6172530f27e2246
Mesrop Kareyan <Mesrop@Mesbook.local>
2017-01-14 13:28:08 +0400

    first commit

---
+++ app_1/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  app_1
+//
+//  Created by Mesrop Kareyan on 1/14/17.
+//  Copyright © 2017 Mesrop Kareyan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: NSObject {
+
+}
